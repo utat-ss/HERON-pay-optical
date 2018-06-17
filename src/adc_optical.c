f1e0e39a849abf7ac38cb253c6f6980631b6323f
Bruno Almeida <bruno.fa.almeida@gmail.com>
2018-06-17 01:49:20 -0400

    Refactor optical ADC library

--- src/adc_optical.c
+++ src/adc_optical.c
@@ -5,24 +5,13 @@ AUTHORS: Bruno Almeida, Dylan Vogel
 This is referred to as the "optical ADC", which is different from the primary
 ADC library in lib-common.
 
+- single conversion mode
+
 TODO:
-* Consider implementing CS is hold low for conversion process
 * Add a function to automatically scale gain
 * Add calibration function
-* Adapt to handle multiple ADCs
-* Fix the CS and hardware address issues
-
-Configuration register bits:
-* bit[23] to 0 - chop bit
-* bit[20] set to 0 — ref select
-* bit[18] should be 1 (psuedo bit)
-* bit[16] set to 0 — temp bit
-* bit[15:8] – channel select
-* bit[7] set to 0 – burnout
-* bit[6] set to 0 - ref detect
-* bit[4] set to 0 – buffer, might want to enable
-* bit[3] set to 1 - unipolar or bipolar (1 for unipolar)
-* bit[2:0] set to 000 - programmable gain
+* Use 4 GPIO pins for mux A0, A1, A2, EN
+* Switch GPIO outputs to mux when appropriate, create function
 */
 
 #include "adc_optical.h"
@@ -35,46 +24,179 @@ void adc_optical_init(void){
 
     // adc_optical_write_register(CONFIG_ADDR, CONFIG_DEFAULT);
     // "Continuous conversion is the default power-up mode." (p. 32)
+
+    // TODO - start in power down mode
+
+    // GPOCON register - enable 4 GPIO outputs
+    // _EN_ = 1, A2/A1/A0 = 0
+    adc_optical_write_register(GPOCON_ADDR, 0b00111000);
+
+    adc_optical_init_config();
+    adc_optical_init_mode();
+}
+
+
+// Initializes configuration register
+void adc_optical_init_config(void) {
+    uint32_t config = adc_optical_read_register(CONFIG_ADDR);
+
+    // Enable pseudo-differential output
+    // Enable unipolar (positive voltage) mode
+    // Set gain to 0b000 (PGA = 1)
+    config = config | CONFIG_PSEUDO;
+    config = config | CONFIG_UNIPOLAR;
+    config = config & 0xFFFFF8;
+
+    adc_optical_write_register(CONFIG_ADDR, config);
+}
+
+
+// Initializes mode register
+void adc_optical_init_mode(void) {
+    uint32_t mode = adc_optical_read_register(MODE_ADDR);
+
+    // Clear first 3 bits and set operating mode to power down mode
+    mode = mode & 0x1FFFFF;
+    mode = mode | MODE_POWER_DOWN;
+
+    adc_optical_write_register(MODE_ADDR, mode);
 }
 
 
 uint32_t adc_optical_read_register(uint8_t register_addr) {
     // Read the current state of the specified ADC register.
 
+    register_addr = register_addr & 0b111;
+
+    // Start communication
     set_cs_low(ADC_OPTICAL_CS_PIN, &ADC_OPTICAL_CS_PORT);
-    send_spi(COMM_BYTE_READ_SINGLE | (register_addr << 3));
+
+    // Begin communication with a write to the communications register
+    // Request a read from the specified register
+    send_spi(COMM_READ | (register_addr << 3));
 
     // Read the required number of bytes based on register
     uint32_t data = 0;
-    for (int i = 0; i < adc_optical_num_register_bytes(register_addr); i++) {
+    for (uint8_t i = 0; i < adc_optical_num_register_bytes(register_addr); i++) {
         data = data << 8;
-        data = data | send_spi(0);
+        data = data | send_spi(0x00);
     }
 
-    // Set CS high
+    // Stop communication
     set_cs_high(ADC_OPTICAL_CS_PIN, &ADC_OPTICAL_CS_PORT);
 
     return data;
 }
 
+
 void adc_optical_write_register(uint8_t register_addr, uint32_t data) {
     // Writes a new state to the specified ADC register.
 
+    register_addr = register_addr & 0b111;
+
+    // Start communication
     set_cs_low(ADC_OPTICAL_CS_PIN, &ADC_OPTICAL_CS_PORT);
-    send_spi(COMM_BYTE_WRITE | (register_addr << 3));
+
+    // Begin communication with a write to the communications register
+    // Request a write to the specified register
+    send_spi(COMM_WRITE | (register_addr << 3));
 
     // Write the number of bytes in the register
-    for (int i = adc_optical_num_register_bytes(register_addr) - 1; i >= 0; i--) {
-        send_spi( (uint8_t)(data >> (i * 8)) );
+    for (int8_t i = adc_optical_num_register_bytes(register_addr) - 1; i >= 0; i--) {
+        send_spi( (uint8_t) (data >> (i * 8)) );
     }
 
     // Set CS high
     set_cs_high(ADC_OPTICAL_CS_PIN, &ADC_OPTICAL_CS_PORT);
 }
 
+
+void adc_optical_select_channel(uint8_t channel_num) {
+    // Sets the configuration register's bits for the specified ADC input channel.
+
+    // Get the 4 bits for the channel for psuedo-differential positive input (p. 26)
+    uint8_t channel_bits = (channel_num - 1) << 4;
+
+    // Read the current register status
+    uint32_t config = adc_optical_read_register(CONFIG_ADDR);
+
+    // Mask the channel bits and write new channel
+    config &= 0xffff00ff;
+    config |= (channel_bits << 8);
+
+    // Write modified config register
+    adc_optical_write_register(CONFIG_ADDR, config);
+}
+
+
+void adc_optical_select_pga(uint8_t gain) {
+    // Sets the configuration register's bits for a specified programmable gain.
+    // gain - one of 1, 8, 16, 32, 64, 128 (2 and 4 are reserved/unavailable, see p. 25)
+
+    // Convert gain to 3 bits
+    uint8_t gain_bits = adc_optical_convert_gain_to_gain_bits(gain);
+
+    // Read from configuration register
+    uint32_t config_data = adc_optical_read_register(CONFIG_ADDR);
+
+    // Clear gain bits and set
+    config_data &= 0xfffff8;
+    config_data |= gain_bits;
+
+    // Write to configuration register
+    adc_optical_write_register(CONFIG_ADDR, config_data);
+}
+
+
+void adc_optical_select_operating_mode(uint32_t mode_bits) {
+    // Read from mode register
+    uint32_t mode_reg = adc_optical_read_register(MODE_ADDR);
+
+    // Clear gain bits and set
+    mode_reg &= 0x1fffff;
+    mode_reg |= mode_bits;
+
+    // Write to configuration register
+    adc_optical_write_register(MODE_ADDR, mode_reg);
+}
+
+
+uint32_t adc_optical_read_raw_data(uint8_t channel_num, uint8_t gain) {
+    // Reads 24 bit raw data from the specified ADC channel.
+
+    // Select the channel for conversion
+    adc_optical_select_channel(channel_num);
+    adc_optical_select_pga(gain);
+    adc_optical_select_operating_mode(MODE_SINGLE_CONV);
+
+    // Wait until the conversion finishes, signalled by (DOUT/_RDY_/MISO) going low
+    // TODO: add a timeout
+    uint16_t timeout = 1023;
+    while (bit_is_set(PINB, MISO_PIN) && timeout > 0) {
+        timeout--;
+    }
+
+    // Read back the conversion result
+    return adc_optical_read_register(DATA_ADDR);
+}
+
+
+double adc_optical_convert_raw_data_to_voltage(uint32_t raw_data, uint8_t gain) {
+    // Convert a raw 24 bit measurement to the input voltage (in V) for the specified
+    // ADC channel, including applying the gain factor.
+
+    // (p.31) Code = (2^N * AIN * Gain) / (V_REF)
+    //     => AIN = (Code * V_REF) / (2^N * Gain)
+    double num = ((double) raw_data) * V_REF;
+    double denom = (((uint32_t) 1) << N) * gain;
+    double AIN = num / denom;
+
+    return AIN;
+}
+
+
 uint8_t adc_optical_num_register_bytes(uint8_t register_addr) {
     // Returns the number of BYTES in the specified register at the given address
-    // TODO - careful of data register + status information - pg. 20
 
     switch (register_addr) {
         case STATUS_ADDR:
@@ -88,6 +210,7 @@ uint8_t adc_optical_num_register_bytes(uint8_t register_addr) {
             break;
         case DATA_ADDR:
             return 3;
+            // TODO - check byte of status information (enabled by setting DAT_STA, p. 20-21)
             break;
         case ID_ADDR:
             return 1;
@@ -107,92 +230,45 @@ uint8_t adc_optical_num_register_bytes(uint8_t register_addr) {
     }
 }
 
-void adc_optical_select_channel(uint8_t channel_num) {
-    // Sets the configuration register's bits for the specified ADC input channel.
-    // channel_num - one of 5, 6, 7
 
-    // Get the 4 bits for the channel (p. 26)
-    uint8_t channel_bits = channel_num - 1;
-
-    // Read the current register status
-    uint32_t config_data = adc_optical_read_register(CONFIG_ADDR);
-
-    // Mask the channel bits and write new channel
-    config_data &= 0xffff00ff;
-    config_data |= (channel_bits << 12);
-
-    // Write modified config register
-    adc_optical_write_register(CONFIG_ADDR, config_data);
-}
-
-uint32_t adc_optical_read_channel(uint8_t channel_num) {
-    // Read 24 bit raw data from the specified ADC channel.
-    // channel_num - one of 5, 6, 7
-
-    uint32_t read_data;
-
-    // Select the channel for conversion
-    adc_optical_select_channel(channel_num);
-
-    // Wait until the conversion finishes, signalled by MISO going high
-    // BUG: conversion is finished when MISO goes *LOW* - fixed
-    // TODO: add a timeout
-    while (bit_is_set(PINB, PB0)){
-        continue;
-    }
-
-    // Read back the conversion result
-    read_data = adc_optical_read_register(DATA_ADDR);
-
-    return read_data;
-}
-
-
-double adc_optical_convert_reading(uint32_t ADC_reading, uint8_t pga_gain) {
-    // Reads the input voltage for the specified ADC channel,
-    // including applying the gain factor.
-    // channel_num - one of 5, 6, 7
-
-
-    // (p.31) Code = (2^N * AIN * Gain) / (V_REF)
-    //     => AIN = (Code * V_REF) / (2^N * Gain)
-    double num = ADC_reading * V_REF;
-    double denom = (((uint32_t) 1) << N) * pga_gain;
-    double AIN = num / denom;
-
-    // return AIN;
-    return AIN;
-}
-
-
-uint8_t adc_optical_convert_gain_bits(uint8_t gain) {
+uint8_t adc_optical_convert_gain_to_gain_bits(uint8_t gain) {
     // Converts the numerical gain to 3 gain select bits (p.25).
-    // gain - one of 1, 8, 16, 32, 64, 128
+    // gain - one of { 1, 8, 16, 32, 64, 128 }
 
     switch (gain) {
         case 1:
-            return 0x00;
+            return 0b000;
             break;
         case 8:
-            return 0x03;
+            return 0b011;
             break;
         case 16:
-            return 0x04;
+            return 0b100;
             break;
         case 32:
-            return 0x05;
+            return 0b101;
             break;
         case 64:
-            return 0x06;
+            return 0b110;
             break;
         case 128:
-            return 0x07;
+            return 0b111;
             break;
         default:
-            return 0x00;
+            return 0b000;
             break;
     }
 }
+
+
+
+
+// TODO
+/*
+    // Calibrate the ADC and re-enable continuous conversion mode
+    calibrate_adc(SYS_ZERO_SCALE_CALIB);
+    calibrate_adc(SYS_FULL_SCALE_CALIB);
+    enable_cont_conv_mode();
 
 void adc_optical_calibrate(uint8_t mode_select_bits) {
     // TODO - NOT TESTED YET
@@ -218,55 +294,4 @@ void adc_optical_calibrate(uint8_t mode_select_bits) {
         continue;
     }
 }
-
-void adc_optical_enable_cont_conv_mode(void) {
-    // Continuous conversion mode
-
-    // Read from mode register
-    uint32_t mode_data = adc_optical_read_register(MODE_ADDR);
-
-    // Clear and set mode select bits
-    mode_data &= 0xff1fffff;
-    mode_data |= ( ((uint32_t) CONT_CONV) << 21 );
-
-    // Write to mode register
-    adc_optical_write_register(MODE_ADDR, mode_data);
-}
-
-/*void set_PGA(uint8_t gain) {
-    // Sets the configuration register's bits for a specified programmable gain.
-    // gain - one of 1, 8, 16, 32, 64, 128 (2 and 4 are reserved/unavailable, see p. 25)
-
-    // Convert gain to 3 bits
-    uint8_t gain_bits = adc_optical_convert_gain_bits(gain);
-
-    uint8_t gain = 128;
-
-    set_PGA(gain);
-
-    // Write to configuration register
-    adc_optical_write_register(CONFIG_ADDR, config_data);
-
-    // Calibrate the ADC and re-enable continuous conversion mode
-    calibrate_adc(SYS_ZERO_SCALE_CALIB);
-    calibrate_adc(SYS_FULL_SCALE_CALIB);
-    enable_cont_conv_mode();
-}*/
-
-void adc_optical_set_pga(uint8_t gain) {
-    // Sets the configuration register's bits for a specified programmable gain.
-    // gain - one of 1, 8, 16, 32, 64, 128 (2 and 4 are reserved/unavailable, see p. 25)
-
-    // Convert gain to 3 bits
-    uint8_t gain_bits = adc_optical_convert_gain_bits(gain);
-
-    // Read from configuration register
-    uint32_t config_data = adc_optical_read_register(CONFIG_ADDR);
-
-    // Clear gain bits and set
-    config_data &= 0xfffffff8;
-    config_data |= gain_bits;
-
-    // Write to configuration register
-    adc_optical_write_register(CONFIG_ADDR, config_data);
-}
+*/
--- src/adc_optical.h
+++ src/adc_optical.h
@@ -2,21 +2,20 @@
 #define ADC_OPTICAL_H
 
 #include <spi/spi.h>
-#include <pex/pex.h>
 #include <stdint.h>
+
+
+
 
 #define ADC_OPTICAL_CS_PIN  PB6
 #define ADC_OPTICAL_CS_PORT PORTB
 #define ADC_OPTICAL_CS_DDR  DDRB
 
-// For writing to the communication register
-// (when starting a communication operation with the ADC)
-// to specify whether the following operation will be read or write
-#define COMM_BYTE_WRITE       0b00000000
-#define COMM_BYTE_READ_CONT   0b01000100  // CREAD is set
-#define COMM_BYTE_READ_SINGLE 0b01000000  // single conversion
-// bit[5:3] is register address
-// bit[2] is CREAD. Set to 1 to enable continuous read
+#define MISO_PIN    PB0
+#define MISO_PORT   PORTB
+#define MISO_DDR    DDRB
+
+
 
 // Register addresses (p.20)
 #define STATUS_ADDR     0x00
@@ -28,10 +27,31 @@
 #define OFFSET_ADDR     0x06
 #define FULL_SCALE_ADDR 0x07
 
-#define CONFIG_DEFAULT  0x040008
+// Communications register
+#define COMM_WRITE  (0 << 6)
+#define COMM_READ   (1 << 6)
 
-// Current default mode settings are alright.
-// #define MODE_DEFAULT    0x00
+/*
+TODO - check configuration register bits:
+* bit[23] to 0 - chop bit
+* bit[20] set to 0 — ref select
+* bit[18] should be 1 (psuedo bit)
+* bit[16] set to 0 — temp bit
+* bit[15:8] – channel select
+* bit[7] set to 0 – burnout
+* bit[6] set to 0 - ref detect
+* bit[4] set to 0 – buffer, might want to enable
+* bit[3] set to 1 - unipolar or bipolar (1 for unipolar)
+* bit[2:0] set to 000 - programmable gain
+*/
+#define CONFIG_PSEUDO   ( ((uint32_t) 1) << 18 )
+#define CONFIG_UNIPOLAR ( ((uint32_t) 1) << 3  )
+
+#define MODE_SINGLE_CONV    ( ((uint32_t) 0b001) << 21 )
+#define MODE_POWER_DOWN     ( ((uint32_t) 0b011) << 21 )
+
+
+
 
 // For ADC read conversion (p.31)
 #define N 24      // number of bits read
@@ -45,16 +65,23 @@
 #define SYS_FULL_SCALE_CALIB  0x7
 
 
+
+
 void adc_optical_init(void);
+void adc_optical_init_config(void);
+void adc_optical_init_mode(void);
+
 uint32_t adc_optical_read_register(uint8_t register_addr);
 void adc_optical_write_register(uint8_t register_addr, uint32_t data);
-uint8_t adc_optical_num_register_bytes(uint8_t register_addr);
+
 void adc_optical_select_channel(uint8_t channel_num);
-uint32_t adc_optical_read_channel(uint8_t channel_num);
-double adc_optical_convert_reading(uint32_t ADC_reading, uint8_t pga_gain);
-uint8_t adc_optical_convert_gain_bits(uint8_t gain);
-void adc_optical_calibrate(uint8_t mode_select_bits);
-void adc_optical_enable_cont_conv_mode(void);
-void adc_optical_set_pga(uint8_t gain);
+void adc_optical_select_pga(uint8_t gain);
+void adc_optical_select_operating_mode(uint32_t mode_bits);
+
+uint32_t adc_optical_read_raw_data(uint8_t channel_num, uint8_t gain);
+double adc_optical_convert_raw_data_to_voltage(uint32_t raw_data, uint8_t gain);
+
+uint8_t adc_optical_num_register_bytes(uint8_t register_addr);
+uint8_t adc_optical_convert_gain_to_gain_bits(uint8_t gain);
 
 #endif
