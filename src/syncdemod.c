fb7fa662f6db0bcb4f576c0d1d3bc5343e02e514
vogeldylan <vogel.sohei@gmail.com>
2018-09-06 11:27:09 -0400

    Added a couple of functions to sync-demod control
    - Reset toggle function
    - Filter coeff setting functions
    - Rclk enable/disable functions[MaP2
    - External clock enable/disable functions
    - SYNCO register writing function
    - Clock register (and human-friendly clock conversion) writing function
    
    Code is untested

--- src/syncdemod.c
+++ src/syncdemod.c
@@ -1,32 +1,38 @@
 /*
-DESCRIPTION: ADA2200 functions
-AUTHORS: Dylan Vogel
+    DESCRIPTION: ADA2200 functions
+    AUTHORS: Dylan Vogel
 
-Contains functions for interfacing with the ADA2200 synrchonous demodulator on
-the pay-optical PCB
+    Contains functions for interfacing with the ADA2200 synrchonous demodulator on
+    the pay-optical PCB
 
-TODO:
-* Implement syncdemod_enable_rclk
-* Implement functions to just set bandpass filter
-* Consider implementing a reset toggle function, since reset can be held low for
-  an indefinite amount of time.
+    TODO:
+    -   Implement functions to just set bandpass filter coefficients (i.e. wtf are they)
 */
+
+#include "syncdemod.h"
 
 void syncdemod_init(void){
     uint8_t sd_cs_pin;
     // SD CS's are PC3 to PC6
     for (int i = 0; i < 4; i++){
         sd_cs_pin = i + 3;
+        // initialize the SPI pin behaviour
         init_cs(sd_cs_pin, &SD_CS_DDR);
         set_cs_high(sd_cs_pin, &SD_CS_PORT);
-        // Write the default configuration
+        // Write the default configuration register
         syncdemod_write_register(sd_cs_pin, SD_SERIAL_CONFIG_ADDR, SD_SERIAL_CONFIG_DEFAULT);
+        // disable the RCLK output
+        syncdemod_disable_rclk(sd_cs_pin);
+        // write the default demod control, which controls RCLK pin behaviour
+        syncdemod_write_register(sd_cs_pin, SD_DEMOD_CONTROL_ADDR, SD_DEMOD_CONTROL_DEFAULT);
+        // enable the external clock input
+        syncdemod_enable_external_clk(sd_cs_pin);
     }
 }
+
 void syncdemod_write_register(uint8_t sd_cs_pin, uint16_t addr, uint8_t data){
-    uint16_t command;
     // Write is indicated by a 0 in bit 15. Read would be 1 in bit 15 (pg 19/24)
-    command = 0x7FFF & addr;
+    uint16_t command = 0x7FFF & addr;
     // Four different SDs they could write to.
     // User should specify SD1, SD2, SD3 or SD4
     set_cs_low(sd_cs_pin, &SD_CS_PORT);
@@ -44,32 +50,113 @@ void syncdemod_write_multibyte(uint8_t sd_cs_pin, uint16_t addr, uint8_t len, ui
     command = 0x7FFF & addr;
 
     set_cs_low(sd_cs_pin, &SD_CS_PORT);
-    send_spi((uint8_t)(command >> 8 ));
+
+    send_spi((uint8_t)(command >> 8));
     send_spi((uint8_t)(command));
     for (int i = 0; i < len; i++){
         send_spi(data[i]);
     }
+
+    set_cs_high(sd_cs_pin, &SD_CS_PORT);
 }
 
-void syncdemod_set_filters(uint8_t* config_arr){
+void syncdemod_set_filters(uint8_t sd_cs_pin, uint8_t* config_arr){
+    // Write 0x03 to filter strobe register to load the coefficients
+    uint16_t command = 0x03;
+    // Write the filter coefficients
+    syncdemod_write_multibyte(sd_cs_pin, SD_FILTER_CONFIG_BASE_ADDR, 23, config_arr);
+    // Get the filters set
+    syncdemod_write_register(sd_cs_pin, SD_FILTER_STROBE, command);
+    // write 0x00 to reset the strobe register (might not be needed?)
+    command = 0x00;
+    syncdemod_write_register(sd_cs_pin, SD_FILTER_STROBE, command);
 
 }
 
-void syncdemod_enable_rclk(void){
+void syncdemod_set_clock(uint8_t sd_cs_pin, int clkin_div, uint8_t rclk_div){
+    uint8_t clkin_bits, rclk_bits, command;
 
+    switch(clkin_div){
+        // default (on reset) is clkin div == 1
+        case 1:
+            clkin_bits = 0x00;
+        case 16:
+            clkin_bits = 0x01;
+        case 64:
+            clkin_bits = 0x02;
+        case 256:
+            clkin_bits = 0x04;
+        default:
+            clkin_bits = 0x00;
+    }
+
+    switch (rclk_div){
+        // default (on reset) is rclk div == 8
+        case 4:
+            rclk_bits = 0x01;
+        case 8:
+            rclk_bits = 0x02;
+        default:
+            rclk_bits = 0x02;
+    }
+
+    command = (clkin_bits << 2) | rclk_bits;
+    syncdemod_write_register(sd_cs_pin, SD_CLK_CONFIG_ADDR, command);
 }
-void syncdemod_disable_rlk(void){
 
+void syncdemod_write_synco(uint8_t sd_cs_pin, uint8_t enable, uint8_t polarity, uint8_t edge){
+    // bit 5 is synco enable (active high)
+    // bit 4 is polarity (default == 0)
+    // bit[3:0] is edge selection for the synco output (default == 1101)
+    command = ((0x01 & enable) << 5) | ((0x01 & polarity) << 4) | (0x0F edge)
+}
+
+void syncdemod_enable_external_clk(uint8_t sd_cs_pin){
+    // enables CMOS level clock on the CLKIN pin
+    uint16_t command = 0x01;
+    syncdemod_write_register(sd_cs_pin, SD_ANALOG_CONFIG_ADDR, command)
+}
+
+void syncdemod_disable_external_clk(uint8_t sd_cs_pin){
+    // disables the CMOS level clock and enables the external crystal
+    uint16_t command = 0x00;
+    syncdemod_write_register(sd_cs_pin, SD_ANALOG_CONFIG_ADDR, command)
+}
+
+void syncdemod_enable_rclk(uint8_t sd_cs_pin){
+    // set bit 0 to 1 to enable RCLK/SDO pin
+    uint16_t command = 0x01;
+    syncdemod_write_register(sd_cs_pin, SD_DIGITAL_CONFIG_ADDR, command);
+}
+
+void syncdemod_disable_rclk(uint8_t sd_cs_pin){
+    // Set bit 0 to 0 to disable RCLK/SDO pin
+    uint16_t command = 0x00;
+    syncdemod_write_register(sd_cs_pin, SD_DIGITAL_CONFIG_ADDR, command);
 }
 
 void syncdemod_reset(uint8_t sd_cs_pin){
-    // Resets the specified device registers
-
+    // Resets the specified device
     uint16_t command;
     // Setting bit 7 and bit 0 to 1 resets the device
     command = SD_SERIAL_CONFIG_DEFAULT | (0b10000001);
     syncdemod_write_register(sd_cs_pin, SD_SERIAL_CONFIG_ADDR, command);
     _delay_ms(1);
-    command &= (0b01111110);
+    command = SD_SERIAL_CONFIG_DEFAULT;
+    syncdemod_write_register(sd_cs_pin, SD_SERIAL_CONFIG_ADDR, command);
+}
+
+void syncdemod_toggle_reset(uint8_t sd_cs_pin){
+    // if reset_state == 0, device is not reset, and vice-versa
+    static uint8_t reset_state = 0x00;
+    uint8_t command;
+    switch (reset_state){
+        case 0x00:
+            reset_state = 0x01;
+            command = SD_SERIAL_CONFIG_DEFAULT | (0b10000001);
+        case 0x01:
+            reset_state = 0x00;
+            command = SD_SERIAL_CONFIG_DEFAULT;
+    }
     syncdemod_write_register(sd_cs_pin, SD_SERIAL_CONFIG_ADDR, command);
 }
--- src/syncdemod.h
+++ src/syncdemod.h
@@ -1,3 +1,10 @@
+#ifndef SYNCDEMOD_H
+#define SYNCDEMOD_H
+
+#include <uart/uart.h>
+#include <stdint.h>
+#include <spi/spi.h>
+
 #define SD_CS_PORT  PORTC
 #define SD_CS_DDR   DDRC
 #define SD1_CS_PIN  PC3
@@ -7,19 +14,34 @@
 
 #define SD_SERIAL_CONFIG_ADDR       0x0000
 #define SD_FILTER_STROBE_ADDR       0x0010
-#define SD_FILTER_CONFIG_BASE_ADDR  0x0010
+#define SD_FILTER_CONFIG_BASE_ADDR  0x0011
 #define SD_ANALOG_CONFIG_ADDR       0x0028
 #define SD_SYNC_CONTROL_ADDR        0x0029
 #define SD_DEMOD_CONTROL_ADDR       0x002A
-#define SD_CLCK_CONFIG_ADDR         0x002B
+#define SD_CLK_CONFIG_ADDR         0x002B
 #define SD_DIGITAL_CONFIG_ADDR      0x002C
 #define SD_CORE_RESET_ADDR          0x002D
 
 // No reset, MSB first, addr increment, 3-wire SPI
 #define SD_SERIAL_CONFIG_DEFAULT    0x00
+// bit 6 is phase of RCLK, bit 4 is mixer enable, bit 3 is SDO/RCLK routing, bit[2:0] is VOCM config
+// bit 6 == 1 delays phase of RCLK
+// bit 4 == 1 holds the last sample while RCLK inactive
+// bit 3 == 1 sends RCLK to output
+// bit[2:0] == 000 sets VOCM to VDD/2 low power mode
+#define SD_DEMOD_CONTROL_DEFAULT    0x18
 
 void syncdemod_init(void);
 void syncdemod_write_register(uint16_t addr, uint8_t data);
+void syncdemod_write_multibyte(uint8_t sd_cs_pin, uint16_t addr, uint8_t len, uint8_t* data);
+void syncdemod_set_filters(uint8_t sd_cs_pin, uint8_t* config_arr);
+void syncdemod_set_clock(uint8_t sd_cs_pin, int clkin_div, uint8_t rclk_div);
+void syncdemod_write_synco(uint8_t sd_cs_pin, uint8_t enable, uint8_t polarity, uint8_t edge);
+void syncdemod_enable_external_clk(uint8_t sd_cs_pin);
+void syncdemod_disable_external_clk(uint8_t sd_cs_pin);
 void syncdemod_enable_rclk(void);
 void syncdemod_disable_rclk(void);
 void syncdemod_reset(void);
+void syncdemod_toggle_reset(uint8_t sd_cs_pin);
+
+#endif
