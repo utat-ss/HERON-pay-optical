86916a874e5080e113906f7da9b7687711f93248
auderian <andrew.uderian@hotmail.com>
2018-12-20 10:48:29 -0500

    new: added code to explicitly set the enable pin on channel 5

--- func_tests/optical_syncdemod_func_test/optical_syncdemod_func_test.c
+++ func_tests/optical_syncdemod_func_test/optical_syncdemod_func_test.c
@@ -10,6 +10,10 @@
 #include "../../src/optical_adc.h"
 #include "../../src/pwm.h"
 
+#define CH5_EN_PORT		PORTC
+#define CH5_EN_DDR		DDRC
+#define CH5_EN			PIN2
+
 int main(void){
     init_uart();
     print("\n\nUART initialized\n");
@@ -17,13 +21,20 @@ int main(void){
     init_spi();
     print("SPI initialized\n");
 
+    init_cs(CH5_EN, &CH5_EN_DDR);
+    set_cs_high(CH5_EN, &CH5_EN_PORT);
+    print("Disabled channel 5\n");
+
     opt_adc_init();
     print("Optical ADC initialized\n");
 
     // 160KHz, 50% duty input signal to the syncdemod
     // (8MHz / 50 = 160KHz)
     // should result in an RCLK of 2.5KHz
-    init_pwm_16bit(0, 0x32, 0x16);
+
+    // Changed to 33KHz setpoint
+    // Observed about 513 Hz with almost exactly 50% duty cycle
+    init_pwm_16bit(0, 0xF7, 0x7B);
     print("16-bit PWM initialized\n");
 
     print("\nStarting test\n\n");
