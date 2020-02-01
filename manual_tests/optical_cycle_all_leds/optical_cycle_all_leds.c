0727478236bf8dc4e37f574ccab81d2d38930849
Bruno Almeida <brunofalmeida@users.noreply.github.com>
2020-02-01 18:52:22 -0500

    Update LED cycling test with more patterns

--- manual_tests/optical_cycle_all_leds/optical_cycle_all_leds.c
+++ manual_tests/optical_cycle_all_leds/optical_cycle_all_leds.c
@@ -19,6 +19,36 @@ void sequence(void){
     }
 }
 
+void sequence_2(void){
+    while(1){
+		for (uint8_t i = 0; i < 32; i++){
+			set_led(i, PAY_OPTICAL, 1);
+            _delay_ms(200);
+            set_led(i, PAY_OPTICAL, 0);
+            _delay_ms(100);
+            set_led(i, PAY_LED, 1);
+			_delay_ms(200);
+            set_led(i, PAY_LED, 0);
+            _delay_ms(100);
+        }
+    }
+}
+
+void sequence_3(void){
+    while(1){
+		for (uint8_t i = 16; i < 24; i++){
+			set_led(i, PAY_OPTICAL, 1);
+            _delay_ms(200);
+            set_led(i, PAY_OPTICAL, 0);
+            _delay_ms(100);
+            set_led(i, PAY_LED, 1);
+			_delay_ms(200);
+            set_led(i, PAY_LED, 0);
+            _delay_ms(100);
+        }
+    }
+}
+
 void all(void){
     while(1){
         all_on();
@@ -31,5 +61,7 @@ void all(void){
 int main(void) {
     init_board();
     all();
-    //sequence();
+    // sequence();
+    // sequence_2();
+    // sequence_3();
 }
